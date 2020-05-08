da4d40ce278ffb220c00db4c8114f9b6aab20c06
Shourya Mittal <shourya.mittal@guenstiger.de>
2020-05-09 00:15:55 +0530

    B: Major Element in Array

---
+++ MajorElement/MajorElement.java
@@ -0,0 +1,31 @@
+class MajorElement {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0 ; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                int freq = map.get(nums[i]);
+                freq += 1;
+                map.put(nums[i], freq);
+            }
+            else {
+                map.put(nums[i], 1);
+            }
+        }
+    
+        int major = 0;
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            int val = entry.getValue();
+            if( val > (nums.length / 2)) {
+                major = entry.getKey();
+                break;
+            }
+        }
+        
+        return major;
+    }
+
+    public static void main(String ...s) {
+        MajorElement m = new MajorElement();
+        System.out.println(m.majorityElement([2,3,2,3,4,2,2,4]));
+    }
+}
